fc7f920e76dcfca536448e45fcd25eae46a32377
Aleksejs Gordejevs <aleksej@gordejev.lv>
2020-04-05 23:54:18 +0300

    code formatting

--- src/dots.js
+++ src/dots.js
@@ -4,72 +4,73 @@ import React from "react";
 import classnames from "classnames";
 
 const getDotCount = spec => {
-  let dots;
+    let dots;
 
-  if (spec.infinite) {
-    dots = Math.ceil(spec.slideCount / spec.slidesToScroll);
-  } else {
-    dots =
-      Math.ceil((spec.slideCount - spec.slidesToShow) / spec.slidesToScroll) +
-      1;
-  }
+    if (spec.infinite) {
+        dots = Math.ceil(spec.slideCount / spec.slidesToScroll);
+    } else {
+        dots =
+            Math.ceil((spec.slideCount - spec.slidesToShow) / spec.slidesToScroll) +
+            1;
+    }
 
-  return dots;
+    return dots;
 };
 
 export class Dots extends React.PureComponent {
-  clickHandler(options, e) {
-    // In Autoplay the focus stays on clicked button even after transition
-    // to next slide. That only goes away by click somewhere outside
-    e.preventDefault();
-    this.props.clickHandler(options);
-  }
-  render() {
-    let dotCount = getDotCount({
-      slideCount: this.props.slideCount,
-      slidesToScroll: this.props.slidesToScroll,
-      slidesToShow: this.props.slidesToShow,
-      infinite: this.props.infinite
-    });
+    clickHandler(options, e) {
+        // In Autoplay the focus stays on clicked button even after transition
+        // to next slide. That only goes away by click somewhere outside
+        e.preventDefault();
+        this.props.clickHandler(options);
+    }
 
-    // Apply join & split to Array to pre-fill it for IE8
-    //
-    // Credit: http://stackoverflow.com/a/13735425/1849458
-    const { onMouseEnter, onMouseOver, onMouseLeave } = this.props;
-    const mouseEvents = { onMouseEnter, onMouseOver, onMouseLeave };
-    let dots = Array.apply(
-      null,
-      Array(dotCount + 1)
-        .join("0")
-        .split("")
-    ).map((x, i) => {
-      let leftBound = i * this.props.slidesToScroll;
-      let rightBound =
-        i * this.props.slidesToScroll + (this.props.slidesToScroll - 1);
-      let className = classnames({
-        "slick-active":
-          this.props.currentSlide >= leftBound &&
-          this.props.currentSlide <= rightBound
-      });
+    render() {
+        let dotCount = getDotCount({
+            slideCount: this.props.slideCount,
+            slidesToScroll: this.props.slidesToScroll,
+            slidesToShow: this.props.slidesToShow,
+            infinite: this.props.infinite
+        });
 
-      let dotOptions = {
-        message: "dots",
-        index: i,
-        slidesToScroll: this.props.slidesToScroll,
-        currentSlide: this.props.currentSlide
-      };
+        // Apply join & split to Array to pre-fill it for IE8
+        //
+        // Credit: http://stackoverflow.com/a/13735425/1849458
+        const {onMouseEnter, onMouseOver, onMouseLeave} = this.props;
+        const mouseEvents = {onMouseEnter, onMouseOver, onMouseLeave};
+        let dots = Array.apply(
+            null,
+            Array(dotCount + 1)
+                .join("0")
+                .split("")
+        ).map((x, i) => {
+            let leftBound = i * this.props.slidesToScroll;
+            let rightBound =
+                i * this.props.slidesToScroll + (this.props.slidesToScroll - 1);
+            let className = classnames({
+                "slick-active":
+                    this.props.currentSlide >= leftBound &&
+                    this.props.currentSlide <= rightBound
+            });
 
-      let onClick = this.clickHandler.bind(this, dotOptions);
-      return (
-        <li key={i} className={className}>
-          {React.cloneElement(this.props.customPaging(i), { onClick })}
-        </li>
-      );
-    });
+            let dotOptions = {
+                message: "dots",
+                index: i,
+                slidesToScroll: this.props.slidesToScroll,
+                currentSlide: this.props.currentSlide
+            };
 
-    return React.cloneElement(this.props.appendDots(dots), {
-      className: this.props.dotsClass,
-      ...mouseEvents
-    });
-  }
+            let onClick = this.clickHandler.bind(this, dotOptions);
+            return (
+                <li key={i} className={className}>
+                    {React.cloneElement(this.props.customPaging(i), {onClick})}
+                </li>
+            );
+        });
+
+        return React.cloneElement(this.props.appendDots(dots), {
+            className: this.props.dotsClass,
+            ...mouseEvents
+        });
+    }
 }
